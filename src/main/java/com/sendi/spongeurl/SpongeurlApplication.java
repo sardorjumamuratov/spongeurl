8113de149c4eb0e6084801c76aa7095110fa5937
sardorjumamuratov <sardorjumamuratov23@gmail.com>
2024-09-08 18:36:05 +0100

    first commit

---
+++ src/main/java/com/sendi/spongeurl/SpongeurlApplication.java
@@ -0,0 +1,13 @@
+package com.sendi.spongeurl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpongeurlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpongeurlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sendi/spongeurl/SpongeurlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sendi.spongeurl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpongeurlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
